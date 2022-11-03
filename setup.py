a8e01be987ffe86843593217d852f88e97a35f1a
macrosann <31016530+MACROSANN@users.noreply.github.com>
2022-11-03 16:16:02 -0300

    Atualização SETU

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp Generation Tech Unimed-BH. E-mail:karinatkato@gmail.com.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/MACROSANN/package-template/tree/main",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
